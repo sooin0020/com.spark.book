ed4164c0a4b240b34c6681c365f2f3bcbe702736
spark <sooin0020@gmail.com>
2021-08-29 01:04:00 +0900

    글 등록 화면 접속 수정

--- src/main/java/com/spark/book/springboot/web/IndexController.java
+++ src/main/java/com/spark/book/springboot/web/IndexController.java
@@ -3,10 +3,12 @@ package com.spark.book.springboot.web;
 import com.spark.book.springboot.config.auth.LoginUser;
 import com.spark.book.springboot.config.auth.dto.SessionUser;
 import com.spark.book.springboot.service.posts.PostsService;
+import com.spark.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpSession;
 
@@ -19,27 +21,27 @@ public class IndexController {
 //        return "index";
 //    }
 
-//    @GetMapping("/posts/save")
-//    public String postsSave(){
-//        return "posts-save";
-//    }
-//
-//    private final PostsService postsService;
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
+    private final PostsService postsService;
 
 //    @GetMapping("/")
 //    public String index(Model model){
 //        model.addAttribute("posts", postsService.findAllDesc());
 //        return "index";
 //    }
-//    @GetMapping("/posts/update/{id}")
-//    public String postsUpdate(@PathVariable Long id, Model model){
-//        PostsResponseDto dto = postsService.findById(id);
-//        model.addAttribute("post", dto);
-//
-//        return "posts-update";
-//    }
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
 
-    private final PostsService postsService;
+        return "posts-update";
+    }
+
+//    private final PostsService postsService;
     private final HttpSession httpSession;
 
     @GetMapping("/")
